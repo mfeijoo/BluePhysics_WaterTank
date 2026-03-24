9dfdf255f37e8a6f48f64d410fd6b9479c837778
mfeijoo <mfeijoo@bluephysicsmed.com>
2026-03-23 20:53:46 -0400

    changing values fo XStep and XDIR

--- firmwareESP32S3/firmwareESP32S3.ino
+++ firmwareESP32S3/firmwareESP32S3.ino
@@ -23,8 +23,8 @@ float PSV;
 
 
 // =================== STEPPER PINS ===================
-static const int X_STEP = 19;
-static const int X_DIR  = 20;
+static const int X_STEP = 17;
+static const int X_DIR  = 18;
 
 static const int Y_STEP = 47;
 static const int Y_DIR  = 14;
